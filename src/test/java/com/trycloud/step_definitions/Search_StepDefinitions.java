7efa686e070a1fd0531178eabca5fd315b78f6ba
morb1k <93850423+morb1k@users.noreply.github.com>
2023-07-21 09:41:26 -0400

    Added Snippets

--- src/test/java/com/trycloud/step_definitions/Search_StepDefinitions.java
+++ src/test/java/com/trycloud/step_definitions/Search_StepDefinitions.java
@@ -5,6 +5,106 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class Search_StepDefinitions {
+    @Given("I am a user logged in on the Dashboard")
+    public void i_am_a_user_logged_in_on_the_dashboard() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("I have a file with a known name")
+    public void i_have_a_file_with_a_known_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I click on the magnifying glass icon inside any Module")
+    public void i_click_on_the_magnifying_glass_icon_inside_any_module() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I enter the known name into the search field")
+    public void i_enter_the_known_name_into_the_search_field() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I press the Enter key")
+    public void i_press_the_enter_key() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("I should see a list of search results matching the known name")
+    public void i_should_see_a_list_of_search_results_matching_the_known_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the details side page of the file should appear when clicked on it")
+    public void the_details_side_page_of_the_file_should_appear_when_clicked_on_it() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+  //  io.cucumber.junit.UndefinedStepException: The step 'I am a user logged in on the Dashboard' and 6 other step(s) are undefined.
+
+
+    @Given("I have a contact with a known name")
+    public void i_have_a_contact_with_a_known_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the details side page of the contact should appear when clicked on it")
+    public void the_details_side_page_of_the_contact_should_appear_when_clicked_on_it() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    //io.cucumber.junit.UndefinedStepException: The step 'I am a user logged in on the Dashboard' and 6 other step(s) are undefined.
+
+
+
+    @Given("I have a photo with a known name")
+    public void i_have_a_photo_with_a_known_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the details side page of the photo should appear when clicked on it")
+    public void the_details_side_page_of_the_photo_should_appear_when_clicked_on_it() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+   // io.cucumber.junit.UndefinedStepException: The step 'I am a user logged in and viewing a search item's details' and 2 other step(s) are undefined.
+
+
+    @Given("I am a user logged in and viewing a search item's details")
+    public void i_am_a_user_logged_in_and_viewing_a_search_item_s_details() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I click the App icon at the top left corner on the page")
+    public void i_click_the_app_icon_at_the_top_left_corner_on_the_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("I should be redirected to the Dashboard")
+    public void i_should_be_redirected_to_the_dashboard() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+   // io.cucumber.junit.UndefinedStepException: The step 'I am a user logged in on the Dashboard' and 2 other step(s) are undefined.
+
+    @When("I navigate to the Photos Module")
+    public void i_navigate_to_the_photos_module() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("I should be able to see the photos under the Photos Module")
+    public void i_should_be_able_to_see_the_photos_under_the_photos_module() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
 
 }
